9ab560c4c61473efacfd17d0a90fb431ccd113c8
brokenpip3 <brokenpip3@gmail.com>
2024-03-29 20:31:23 +0100

    use 23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "brokenpip3 blog";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     theme = {
