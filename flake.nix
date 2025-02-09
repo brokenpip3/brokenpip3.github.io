1c86ee8d3e7d8e0ee2294de2353287d3fe659700
Luigi Operoso <40476330+brokenpip3@users.noreply.github.com>
2025-02-10 00:06:59 +0100

    update and add a new post (#8)

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "brokenpip3 blog";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     theme = {
-      url = "gitlab:gabmus/hugo-ficurinia/1b986165b84121be4f28382e8c6bc2408c5baa33";
+      url = "gitlab:gabmus/hugo-ficurinia/8ef7af78c9afa0b1097f6854623bfc59b23f40d7";
       flake = false;
     };
   };
